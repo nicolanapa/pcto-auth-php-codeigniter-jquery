e64722bb2433adec789b1735a9e7d4da7079a539
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 15:15:18 +0200

    feat: Imported About and Contact classes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,13 +3,15 @@
 use CodeIgniter\Router\RouteCollection;
 
 use App\Controllers\Home;
+use App\Controllers\About;
+use App\Controllers\Contact;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', [Home::class, "index"]);
-$routes->get("/about", ["About::class", "index"]);
-$routes->get("/contact", ["Contact::class", "index"]);
+$routes->get("/about", [About::class, "index"]);
+$routes->get("/contact", [Contact::class, "index"]);
 
 $routes->get("/login", ["AuthenticationPage::class", "login"]);
 $routes->get("/signup", ["AuthenticationPage::class", "signup"]);

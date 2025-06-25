5eaf125946910fea362322c372de46b10c61ab36
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 15:07:31 +0200

    feat: All routes that will be used in advance
    The Classses in the 'to' parameter have been written as Strings since they do not exist at the moment

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,7 +2,24 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+use App\Controllers\Home;
+
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [Home::class, "index"]);
+$routes->get("/about", ["About::class", "index"]);
+$routes->get("/contact", ["Contact::class", "index"]);
+
+$routes->get("/login", ["AuthenticationPage::class", "login"]);
+$routes->get("/signup", ["AuthenticationPage::class", "signup"]);
+
+$routes->get("/reservedPage/normal", ["ReservedPage::class", "normalUser"]);
+$routes->get("/reservedPage/admin", ["ReservedPage::class", "adminUser"]);
+
+// REST API Routes
+$routes->get("/user", ["User::class", "getAll"]);
+$routes->post("/user", ["User::class", "newUser"]);
+$routes->get("/user/(:segment)", ["User::class", "get"]);
+$routes->put("/user(:segment)", ["User::class", "put"]);
+$routes->delete("/user(:segment)", ["User::class", "delete"]);

1c3a81043d4ab4315e17ebcd1404ecef3ef46c97
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 17:03:47 +0200

    fix: Wrong input name

--- app/Views/partials/loginForm.php
+++ app/Views/partials/loginForm.php
@@ -3,7 +3,7 @@
     <input type="text" name="username" id="username" required minlength="4" maxlength="128">
 
     <label for="password">Password: </label>
-    <input type="password" name="passport" id="password" required minlength="2" maxlength="256">
+    <input type="password" name="password" id="password" required minlength="2" maxlength="256">
 
     <button type="submit" class="login-button">Login!</button>
 </form>

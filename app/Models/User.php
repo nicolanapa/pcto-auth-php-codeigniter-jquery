5534b813a8ee5b58ecc6da89f2c6738c0b819f15
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 16:54:57 +0200

    feat: Methods to get a User / Users and add a User

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,5 +5,55 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class User extends Model {
+    protected $table = "user";
+    protected $allowedFields = ["username", "hashedPassword", "is_admin"];
+    protected $primaryKey = "id";
 
+    public function getUsers() {
+        return $this->findAll();
+    }
+
+    public function getUser($id) {
+        return $this->find($id);
+    }
+
+    public function postUser($data) {
+        $hashedPassword = password_hash($data["password"], PASSWORD_ARGON2ID);
+
+        if ($this->where("username", $data["username"])->first() !== null) {
+            echo "User already exists!";
+
+            return;
+        }
+
+        $this->save([
+            "username" => $data["username"],
+            "hashed_password" => $hashedPassword,
+            "is_admin" => $data["is_admin"]
+        ]);
+
+        echo "Created and Logged In!";
+        echo "Username " . $data["username"] . " " .  $data["password"] . " " . $hashedPassword . " " . $data["is_admin"];
+    }
+
+    public function checkPassword($data) {
+        if ($this->where("username", $data["username"])->first() === null) {
+            echo "User doesn't exist";
+
+            return;
+        }
+
+        if (password_verify(
+            $data["password"],
+            $this->select("hashed_password")->where("username", $data["username"])->first()["hashed_password"]
+        )) {
+            echo "1";
+            
+            return true;
+        } else {
+            echo "0";
+
+            return false;
+        }
+    }
 }

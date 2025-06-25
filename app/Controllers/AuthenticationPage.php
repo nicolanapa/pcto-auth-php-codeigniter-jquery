f3f1f76057c24accb15ec5b66812cf2023a0b44a
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 16:57:04 +0200

    feat: Logic for login and signup from Form data

--- app/Controllers/AuthenticationPage.php
+++ app/Controllers/AuthenticationPage.php
@@ -2,24 +2,50 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
 class AuthenticationPage extends BaseController {
     public function loginView() {
+        helper("form");
+
         return view("partials/head", ["title" => "Login Page"])
             . view("partials/loginForm")
             . view("partials/foot");
     }
 
     public function login() {
-        return "";
+        $data = $this->request->getPost(["username", "password"]);
+
+        // validate
+
+        $model = model(User::class);
+
+        // $user = $this->validator->getValidated();
+
+        $model->checkPassword($data /* $user */);
     }
 
     public function signupView() {
+        helper("form");
+
         return view("partials/head", ["title" => "Signup Page"])
             . view("partials/signupForm")
             . view("partials/foot");
     }
 
     public function signup() {
+        $data = $this->request->getPost(["username", "password", "is_admin"]);
+
+        // validate
+
+        $model = model(User::class);
+
+        // $user = $this->validator->getValidated();
+
+        $model->postUser($data /* $user */);
+
+        echo 1;
+
         return "";
     }
 }

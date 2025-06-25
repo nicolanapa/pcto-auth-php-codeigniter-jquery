8e63249744c518a1f3a5d0b425b1c241f5bb6379
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 16:56:13 +0200

    fix: Incorrect input name and didn't update a button class

--- app/Views/partials/signupForm.php
+++ app/Views/partials/signupForm.php
@@ -3,10 +3,10 @@
     <input type="text" name="username" id="username" required minlength="4" maxlength="128">
 
     <label for="password">Password: </label>
-    <input type="password" name="passport" id="password" required minlength="2" maxlength="256">
+    <input type="password" name="password" id="password" required minlength="2" maxlength="256">
 
     <label for="is-admin">Are you an Admin? </label>
-    <input type="checkbox" name="is_admin" id="is-admin">
+    <input type="checkbox" name="is_admin" id="is-admin" value="true">
 
-    <button type="submit" class="login-button">Login!</button>
+    <button type="submit" class="signup-button">Signup!</button>
 </form>

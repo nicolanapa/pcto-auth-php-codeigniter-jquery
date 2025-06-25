cc6b046bc4e1e156bb6299a1cc738db89cbea07a
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-25 16:00:58 +0200

    feat: Correctly added remaining 'commented' Paths
    Added POST routes for /login and /signup so they can handle the data from the forms

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Home;
 use App\Controllers\About;
 use App\Controllers\Contact;
+use App\Controllers\AuthenticationPage;
+use App\Controllers\ReservedPage;
+use App\Controllers\User;
 
 /**
  * @var RouteCollection $routes
@@ -13,15 +16,17 @@ $routes->get('/', [Home::class, "index"]);
 $routes->get("/about", [About::class, "index"]);
 $routes->get("/contact", [Contact::class, "index"]);
 
-$routes->get("/login", ["AuthenticationPage::class", "login"]);
-$routes->get("/signup", ["AuthenticationPage::class", "signup"]);
+$routes->get("/login", [AuthenticationPage::class, "loginView"]);
+$routes->post("/login", [AuthenticationPage::class, "login"]);
+$routes->get("/signup", [AuthenticationPage::class, "signupView"]);
+$routes->post("/signup", [AuthenticationPage::class, "signup"]);
 
-$routes->get("/reservedPage/normal", ["ReservedPage::class", "normalUser"]);
-$routes->get("/reservedPage/admin", ["ReservedPage::class", "adminUser"]);
+$routes->get("/reservedPage/normal", [ReservedPage::class, "normalUser"]);
+$routes->get("/reservedPage/admin", [ReservedPage::class, "adminUser"]);
 
 // REST API Routes
-$routes->get("/user", ["User::class", "getAll"]);
-$routes->post("/user", ["User::class", "newUser"]);
-$routes->get("/user/(:segment)", ["User::class", "get"]);
-$routes->put("/user(:segment)", ["User::class", "put"]);
-$routes->delete("/user(:segment)", ["User::class", "delete"]);
+$routes->get("/user", [User::class, "getAll"]);
+$routes->post("/user", [User::class, "newUser"]);
+$routes->get("/user/(:segment)", [User::class, "get"]);
+$routes->put("/user(:segment)", [User::class, "put"]);
+$routes->delete("/user(:segment)", [User::class, "delete"]);

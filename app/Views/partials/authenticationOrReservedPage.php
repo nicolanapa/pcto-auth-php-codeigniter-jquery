4d208a50e32a42452d1ce9f4336cf20f8beee2e3
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-26 15:24:02 +0200

    fix: Crash in case there was an Error in  and 'username' doesn't exist

--- app/Views/partials/authenticationOrReservedPage.php
+++ app/Views/partials/authenticationOrReservedPage.php
@@ -2,10 +2,11 @@
 $userSession = new App\Libraries\UserSession();
 
 $data = $userSession->getSession();
+// var_dump($data);
 ?>
 
 <?php if ($userSession->checkIfExists()) { ?>
-    <p><a href="/reservedPage/normal">Reserved Pages (Welcome Back <?= $data["username"] ?>)</a></p>
+    <p><a href="/reservedPage/normal">Reserved Pages (Welcome Back <?= isset($data["username"]) ? $data["username"] : "" ?>)</a></p>
 <?php } else { ?>
     <p><a href="/login">Login</a></p>
     <p><a href="/signup">Signup</a></p>

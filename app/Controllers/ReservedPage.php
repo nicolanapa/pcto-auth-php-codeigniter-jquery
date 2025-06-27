9566de9c3ee7a7be6fe3f43760f1861c5e69f610
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-27 10:43:35 +0200

    feat: Checking of User permissions in both reserved paths

--- app/Controllers/ReservedPage.php
+++ app/Controllers/ReservedPage.php
@@ -2,12 +2,50 @@
 
 namespace App\Controllers;
 
+use App\Libraries\UserSession;
+
 class ReservedPage extends BaseController {
+    protected $user;
+
     public function normalUser() {
-        return "";
+        $sessionHandler = new UserSession();
+
+        if (!$sessionHandler->checkIfExists()) {
+            return redirect()->to("/");
+        }
+
+        $this->user = $sessionHandler->getSession();
+
+        if (!isset($this->user)) {
+            return redirect()->to("/");
+        }
+
+        return view("partials/head", ["title" => "Normal User Reserved Page"])
+            . " NORMAL USER PAGE"
+            . view("partials/foot");
     }
 
     public function adminUser() {
-        return "";
+        $sessionHandler = new UserSession();
+
+        if (!$sessionHandler->checkIfExists()) {
+            return redirect()->to("/");
+        }
+
+        $this->user = $sessionHandler->getSession();
+
+        if (isset($this->user) && isset($this->user["is_admin"])) {
+            if ($this->user["is_admin"] !== "1") {
+                return redirect()->to("/");
+            }
+        } else {
+            return redirect()->to("/");
+        }
+
+
+
+        return view("partials/head", ["title" => "Admin User Reserved Page"])
+            . " ADMIN USER PAGE"
+            . view("partials/foot");
     }
 }

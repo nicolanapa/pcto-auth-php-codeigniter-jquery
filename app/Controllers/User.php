033c0a5e521b296341ff2709ec3558a9391f1e61
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-30 12:32:35 +0200

    feat: First part of PUT /user/:id

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -19,9 +19,41 @@ class User extends BaseController {
         return $this->response->setJSON(new UserModel()->getUser($userId));
     }
 
-    public function put() {
+    private function validateUserData($data, $type, $name) {
+        if ($this->validateData($data, $type === "username"
+            ? [$name => "required|min_length[4]|max_length[128]"]
+            : ($type === "boolean" ? [$name => "is_bool"] : ""))) {
+            return true;
+        } else {
+            $this->response->setStatusCode(400);
+
+            return false;
+        }
     }
 
-    public function delete() {
+    public function put($userId) {
+        $data = $this->request->getRawInputVar(["username", "is_admin", "can_access"]);
+
+        if (isset($data["username"])) {
+            $this->validateUserData($data, "username", "username");
+        } else if (isset($data["is_admin"])) {
+            $this->validateUserData($data, "boolean", "is_admin");
+        } else if (isset($data["can_access"])) {
+            $this->validateUserData($data, "boolean", "can_access");
+        } else {
+            $this->response->setStatusCode(400);
+        }
+
+        $user = $this->validator->getValidated();
+        $user["id"] = $userId;
+
+        return $this->response->setJSON(body: [
+            "status" => $this->response->getStatusCode(),
+            "data" => $user,
+            "errors" => $this->validator->getErrors()
+        ]);
+    }
+
+    public function delete($userId) {
     }
 }

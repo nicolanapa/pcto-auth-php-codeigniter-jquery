bb4319c2eb364907e13385c91c545e21825084c7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-06-30 09:58:51 +0200

    feat: getAll, get methods

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,6 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\User as UserModel;
+
 class User extends BaseController {
-    
+    protected $format = "json";
+    private $model;
+
+    public function getAll() {
+        return $this->response->setJSON(new UserModel()->getUsers());
+    }
+
+    public function post() {
+    }
+
+    public function get($userId) {
+        return $this->response->setJSON(new UserModel()->getUser($userId));
+    }
+
+    public function put() {
+    }
+
+    public function delete() {
+    }
 }

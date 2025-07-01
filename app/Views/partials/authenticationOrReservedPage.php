94c86ede087953a4423655158549cafc8cebf6e7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-07-01 10:40:21 +0200

    feat: Changed most texts to a similar Icon

--- app/Views/partials/authenticationOrReservedPage.php
+++ app/Views/partials/authenticationOrReservedPage.php
@@ -7,12 +7,12 @@ $data["is_admin"] ??= "../";
 ?>
 
 <?php if ($userSession->checkIfExists()) { ?>
-    <p><a href="/reservedPage/<?= $data["is_admin"] === "../" ? $data["is_admin"] : ($data["is_admin"] ? "admin" : "normal") ?>">Reserved Page (Current User: <?= $data["username"] ?? "user" ?>)</a></p>
+    <p><a href="/reservedPage/<?= $data["is_admin"] === "../" ? $data["is_admin"] : ($data["is_admin"] ? "admin" : "normal") ?>"><img src="<?= base_url("./icons/reservedPage.svg") ?>" alt="Contact" width="24px" height="auto"> as <?= $data["username"] ?? "user" ?></a></p>
 
     <form action="/logout" method="post">
-        <button type="submit">Log out</button>
+        <button type="submit" class="logout-button">Log out</button>
     </form>
 <?php } else { ?>
-    <p><a href="/login">Login</a></p>
-    <p><a href="/signup">Signup</a></p>
+    <p><a href="/login"><img src="<?= base_url("./icons/login.svg") ?>" alt="Login" width="32px" height="auto"></a></p>
+    <p><a href="/signup"><img src="<?= base_url("./icons/signup.svg") ?>" alt="Signup" width="32px" height="auto"></a></p>
 <?php } ?>
--- app/Views/partials/navBar.php
+++ app/Views/partials/navBar.php
@@ -1,6 +1,9 @@
 <nav>
-    <p><a href="/">Home</a></p>
-    <p><a href="/about">About</a></p>
-    <p><a href="/contact">Contact</a></p>
-    <?= view("partials/authenticationOrReservedPage") ?>
+    <p><a href="/"><img src="<?= base_url("./icons/home.svg") ?>" alt="Home" width="32px" height="auto"></a></p>
+    <p><a href="/about"><img src="<?= base_url("./icons/about.svg") ?>" alt="About" width="32px" height="auto"></a></p>
+    <p><a href="/contact"><img src="<?= base_url("./icons/contact.svg") ?>" alt="Contact" width="32px" height="auto"></a></p>
+
+    <div class="authentication-reserved">
+        <?= view("partials/authenticationOrReservedPage") ?>
+    </div>
 </nav>

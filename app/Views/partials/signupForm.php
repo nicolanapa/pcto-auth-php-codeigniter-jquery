5c1f9b6ed371f02c45a00db8b88553695872f9f4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-07-07 09:20:51 +0200

    feat: Dynamic path based on signupMode boolean and made the password input always render

--- app/Views/partials/signupForm.php
+++ app/Views/partials/signupForm.php
@@ -2,14 +2,12 @@
 
     <?= validation_list_errors() ?>
 
-    <?= form_open_multipart("/signup") ?>
+    <?= form_open_multipart($signupMode ? "/signup" : "/editAll/" . $user["id"]) ?>
     <label for="username">Username: </label>
     <input type="text" name="username" id="username" required minlength="4" maxlength="128" value="<?= isset($user) ? $user["username"] : "" ?>">
 
-    <?php if ($signupMode) { ?>
-        <label for="password">Password: </label>
-        <input type="password" name="password" id="password" required minlength="2" maxlength="256">
-    <?php } ?>
+    <label for="password">Password: </label>
+    <input type="password" name="password" id="password" required minlength="2" maxlength="256">
 
     <label for="is-admin">Are <?= $signupMode ? "you" : "they" ?> an Admin? </label>
     <input type="checkbox" name="is_admin" id="is-admin" value="true" <?= isset($user) ? ($user["is_admin"] ? "checked" : "") : "" ?>>
@@ -18,7 +16,6 @@
     <input type="checkbox" name="can_access" id="can-access" value="true" <?= isset($user) ? ($user["can_access"] ? "checked" : "") : "false" ?>>
 
     <label for="image">Image</label>
-    <?php /* hidden input with image if it exists */ ?>
     <input type="file" name="image" id="image" accept="image/*" />
 
     <button type="submit" class="signup-button"><?= $signupMode ? "Signup" : "Edit" ?>!</button>
